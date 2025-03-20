91ab8cfeec940c6b088012da25209726d41eaa44
Paloma-PD <mppd03@gmail.com>
2025-03-20 13:49:41 -0600

    check error

--- Challenge_1/upload_results.py
+++ Challenge_1/upload_results.py
@@ -1,6 +1,6 @@
 import os
-print("Directorio actual:", os.getcwd())
+#os.chdir("C:/Users/palom/OneDrive/Dokumen/CUCEA/2DO_SEM/Programacion-2/Challenge_1")
 os.system("git add .")
-os.system("git commit -m 'Tryng file upload_results' ")
+os.system("git commit -m 'Tryng file upload_results'")
 os.system("git push origin main")
 
