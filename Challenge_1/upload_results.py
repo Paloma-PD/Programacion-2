6483e070e8c55f783efba4af21f83ea5d491d6ae
Paloma-PD <mppd03@gmail.com>
2025-03-24 14:15:58 -0600

    cleaning

--- Challenge_1/upload_results.py
+++
@@ -1,6 +0,0 @@
-import os
-#os.chdir("C:/Users/palom/OneDrive/Dokumen/CUCEA/2DO_SEM/Programacion-2/Challenge_1")
-os.system("git add .")
-os.system("git commit -m 'Tryng file upload_results'")
-os.system("git push origin main")
-
